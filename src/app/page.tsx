ad16fbf3d58630a94f0c42ffc74348a0f5993b4b
Hyun <ikimyh059554@gmail.com>
2025-07-12 15:22:40 +0900

    ✨ feat: test page 반응형 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Button>shadcn button</Button>
-      <p className="tablet:text-amber-700 p-[3rem] text-[2.1rem] font-bold">
+      <p className="txt-14-medium text-main md:txt-24-bold md:text-sub lg:txt-32-medium lg:text-red-500">
         프리텐다드 폰트, 반응형, font-size:62.5%, 폴더구조,
         LF설정(윈도우),settings.json,providers,프리티어,
       </p>
