af84114bc7ed74c7499af72143d6330583d06de5
Hyun <ikimyh059554@gmail.com>
2025-07-14 19:07:02 +0900

    ✨ feat: 라우팅 페이지에서 랜딩페이지 호출

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import { Button } from '@/shared/shadcn/components/ui/button';
+'use client';
 
-export default function Home() {
+import LandingPage from '@/features/landing/landing-page';
+
+export default function Page() {
   return (
-    <div>
-      <Button>shadcn button</Button>
-      <p className="txt-14-medium text-main md:txt-24-bold md:text-sub lg:txt-32-medium lg:text-red-500">
-        프리텐다드 폰트, 반응형, font-size:62.5%, 폴더구조,
-        LF설정(윈도우),settings.json,providers,프리티어,
-      </p>
-    </div>
+    <main>
+      <LandingPage />
+    </main>
   );
 }
