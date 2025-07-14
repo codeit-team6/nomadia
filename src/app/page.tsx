2d102d5d78b5922b3eff40ff0824dc3cec909c86
jyn <spdlqjdkslel1226@naver.com>
2025-07-15 01:42:11 +0900

    🎨 style: globals.css - 브랜드 컬러 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import { Button } from '@/shared/shadcn/components/ui/button';
 
 export default function Home() {
   return (
-    <div>
-      <Button>shadcn button</Button>
-      <p className="txt-14-medium text-main md:txt-24-bold md:text-sub lg:txt-32-medium lg:text-red-500">
+    <div className="bg-background h-screen md:bg-amber-100 lg:bg-amber-400">
+      <Button className="bg-red-100">shadcn button</Button>
+      <p className="txt-14-medium text-main md:txt-24-bold md:text-sub lg:txt-32-medium bg-back lg:text-red-500">
         프리텐다드 폰트, 반응형, font-size:62.5%, 폴더구조,
         LF설정(윈도우),settings.json,providers,프리티어,
       </p>
+      <div className="border-sub-300 size-[12rem] rounded-[10rem] border-8 bg-amber-300"></div>
     </div>
   );
 }
