33bfaecaf2c82cd83eae2a4a3f03fb8a3d29d644
wnsdn00 <114475584+wnsdn00@users.noreply.github.com>
2025-07-15 20:34:49 +0900

    [✨Feat] footer 구현 (#12)
    * ✨ Feat: footer 구현
    
    feat: footer 구현
    
    * Update package.json
    
    react 버전 19.1.0 -> 19.0.0
    
    * Update pnpm-lock.yaml
    
    react 버전 19.1.0 -> 19.0.0
    
    * 파일구조 및 확장자 변경
    
    * icons 폴더 추가 및 확장자 변경

---
+++ src/shared/components/footer.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="w-[375px] md:w-[744px] lg:w-[1920px] max-w-[1920px] mx-auto h-[116px] md:h-[140px] py-[30px] px-[24px] md:px-[40px] lg:px-[200px] text-gray-600 bg-white border-t border-gray-100 txt-13-medium">
+      
+      {/* 모바일 사이즈: 위에 보일 Privacy Policy · FAQ */}
+      <div className="flex justify-center gap-7 mb-4 md:hidden">
+        <a href="/privacy-policy">Privacy Policy</a>
+        <span>·</span>
+        <a href="/faq">FAQ</a>
+      </div>
+
+      <div className="flex flex-row justify-between items-center md:grid md:grid-cols-3 md:items-center">
+        
+        <div className="text-gray-400 text-left">
+          ©codeit - 2025
+        </div>
+
+        <div className="hidden md:flex justify-center gap-4">
+          <a href="/privacy-policy">Privacy Policy</a>
+          <span>·</span>
+          <a href="/faq">FAQ</a>
+        </div>
+
+        <div className="flex justify-end items-center gap-[16px]">
+          <img src="/images/icons/facebook.svg" alt="facebook" className="w-[20px] h-[20px]" />
+          <img src="/images/icons/instagram.svg" alt="instagram" className="w-[20px] h-[20px]" />
+          <img src="/images/icons/youtube.svg" alt="youtube" className="w-[20px] h-[20px]" />
+          <img src="/images/icons/x.svg" alt="x" className="w-[20px] h-[20px]" />
+        </div>
+
+      </div>
+    </footer>
+  );
+}
