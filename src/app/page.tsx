6b8d01281ccbdaa1d65685cdfd287d688a3bfc09
wnsdn00 <junwoo000706@gmail.com>
2025-07-15 20:31:19 +0900

    icons 폴더 추가 및 확장자 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
+'use client';
 
 import LandingPage from '@/features/landing/landing-page';
 
 export default function Page() {
   return (
-    <div>
-     
-    </div>
     <main>
       <LandingPage />
     </main>
