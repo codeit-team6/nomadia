93fb6fefaaaf852b9bb4bf905d3fb192784a5206
TopiaYu <raemo963@naver.com>
2025-07-15 18:57:50 +0900

    ✨📁 Zod 데이터 규칙 정하기 및 auth 폴더 생성
    - src/features/auth/validators/auth.schema.ts 에서 zod를 활용하여 데이터 규칙을 설정하였습니다.
    - 이메일과 비밀번호의 유효성을 검사합니다.
    - auth 폴더구조를 생성하였습니다.
    - app
     📦(auth)
     ┣ 📂login
     ┃ ┗ 📜page.tsx
     ┣ 📂signup
     ┃ ┗ 📜page.tsx
    -
    📦features
     ┣ 📂auth
     ┃ ┣ 📂api
     ┃ ┃ ┗ 📜auth.api.ts
     ┃ ┣ 📂login
     ┃ ┃ ┗ 📂components
     ┃ ┃ ┃ ┗ 📜LoginForm.tsx
     ┃ ┣ 📂signup
     ┃ ┃ ┗ 📂components
     ┃ ┃ ┃ ┗ 📜SignupForm.tsx
     ┃ ┣ 📂stores
     ┃ ┃ ┗ 📜useAuthStore.ts
     ┃ ┣ 📂types
     ┃ ┃ ┗ 📜auth.types.ts
     ┃ ┗ 📂validators
     ┃ ┃ ┗ 📜auth.schema.ts
    📦shared
     ┃ ┗ ��libs
     ┃ ┃ ┣ 📂api
     ┃ ┃ ┃ ┗ 📜api.ts

---
+++ src/features/auth/validators/auth.schema.ts
@@ -0,0 +1,23 @@
+import z from 'zod';
+
+export const signupSchema = z
+  .object({
+    email: z.email({ message: '올바른 이메일 형식을 입력해주세요' }),
+    nickname: z.string().min(2, { message: '닉네임은 2자 이상이어야 합니다' }),
+    password: z
+      .string()
+      .min(8, { message: '비밀번호는 8자 이상이어야 합니다' }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: '비밀번호가 일치하지 않습니다',
+    path: ['confirmPassword'],
+  });
+
+export const loginSchema = z.object({
+  email: z.email({ message: '올바른 이메일 형식을 입력해주세요' }),
+  password: z.string().min(8, { message: '비밀번호를 8자 이상 입력해주세요' }),
+});
+
+export type SignupSchema = z.infer<typeof signupSchema>;
+export type LoginSchema = z.infer<typeof loginSchema>;
