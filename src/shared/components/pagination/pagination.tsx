60881f639a30aef431553d0ce5e7279d8a02723e
jyn <spdlqjdkslel1226@naver.com>
2025-07-16 20:02:46 +0900

    📝 docs: 템플릿 수정, JSDoc 작성

--- src/shared/components/pagination/pagination.tsx
+++ src/shared/components/pagination/pagination.tsx
@@ -8,15 +8,48 @@ type PaginationProps = {
   setPage: React.Dispatch<React.SetStateAction<number>>;
 };
 
+/**
+ * 페이지네이션 버튼 UI를 렌더링 함.  
+ * 페이지 버튼을 클릭하면 setPage를 호출하여 상위 컴포넌트에서 선언한 page 상태값을 업데이트 함  
+ * 
+ * 좌측 화살표: 이전 페이지 그룹으로 이동, currentPage를 페이지 그룹의 마지막 요소로 업데이트, 현재페이지가 페이지 그룹 사이즈보다 작으면 비활성화  
+ * 우측 화살표: 다음 페이지 그룹으로 이동, currentPage를 페이지 그룹의 첫번째 요소로 업데이트, 페이지 그룹의 마지막 요소가 totalPages와 같거나 더 크면 비활성화  
+
+ *
+ * @param {number} totalPages - 전체 페이지 수
+ * @param {number} currentPage - 현재 페이지
+ * @param {function} setPage - 현재 페이지를 변경하는 함수 (React 상태 업데이트 함수)
+ *
+ * @example
+ * ```tsx
+ * 'use client';
+ * import { useState } from 'react';
+ * import Pagination from '@/shared/components/pagination/pagination';
+ *
+ * export default function Page() {
+ *   const [page, setPage] = useState(1); //Pagination 컴포넌트의 프롭으로 넘겨주세요
+ *
+ *   useEffect(() => {
+ *     fetchData(page); // 페이지 변경 시 API 호출 등
+ *   }, [page]);
+ *
+ *   return (
+ *     <Pagination totalPages={33} currentPage={page} setPage={setPage} />
+ *   );
+ * }
+ * ```
+ */
+
 const Pagination = ({ totalPages, currentPage, setPage }: PaginationProps) => {
   const PAGE_GROUP_SIZE = 5;
   const pageGroupNum = Math.ceil(currentPage / PAGE_GROUP_SIZE);
-  const pageEnd = Math.min(pageGroupNum * PAGE_GROUP_SIZE);
-  const pageStart = Math.max(1, pageEnd - 4);
+  const startPage = pageGroupNum * PAGE_GROUP_SIZE - PAGE_GROUP_SIZE + 1; //-4
+  const endPage = Math.min(totalPages, startPage + PAGE_GROUP_SIZE - 1); //+4
+
   const pageRange = [];
 
-  for (let i = pageStart; i <= pageEnd; i++) {
-    if (i <= totalPages) pageRange.push(i);
+  for (let i = startPage; i <= endPage; i++) {
+    pageRange.push(i);
   }
 
   const leftDisable = currentPage <= PAGE_GROUP_SIZE;
