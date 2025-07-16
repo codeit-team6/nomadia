0cba671b3c3d79b6f427704a9ca61f076ab5313c
TopiaYu <raemo963@naver.com>
2025-07-16 18:54:17 +0900

    ✨ Feat: refreshToken을 통해 토큰 재발급
    - accessToken 만료시 refreshToken을 사용해 accessToken, refreshToken을 갱신합니다.
    - axios.ts에서 인터셉터를 통해 로직을 구현
    
    - 테스트는 하지 못했습니다..

--- src/features/auth/api/auth.api.ts
+++ src/features/auth/api/auth.api.ts
@@ -15,3 +15,15 @@ export const login = async (data: LoginRequest): Promise<LoginResponse> => {
 export const signup = async (data: SignupRequest): Promise<void> => {
   await api.post('users', data);
 };
+
+//리프레시 토큰을 통한 accessToken 재발급
+export const refreshToken = async (token: string) => {
+  const response = await api.post('/auth/tokens', { refreshToken: token });
+  return response.data;
+};
+
+//리프레시토큰 테스트용 api
+export const getMe = async () => {
+  const response = await api.get('/users/me');
+  return response.data;
+};
--- src/shared/libs/api/axios.ts
+++ src/shared/libs/api/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+import { refreshToken as apiRefreshToken } from '@/features/auth/api/auth.api';
 import { useAuthStore } from '@/features/auth/stores/useAuthStore';
 
 const api = axios.create({
@@ -17,6 +18,56 @@ api.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  },
+);
+
+/**
+ * 토큰 재발급용 인터셉터 입니다
+ *
+ *  @description 요청실패시 응답 처리
+ *  1.요청 실패를 감지합니다
+ *  2.무한루프를 방지합니다
+ *  3.리프레시 토큰이 없다면 바로 로그아웃시켜서 안정성을 높입니다
+ *  4.토큰 갱신을 시도합니다
+ *  5.실패시 로그아웃시킵니다.
+ */
+
+api.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    const originRequest = error.config;
+
+    if (error.response?.status === 401 && !originRequest._retry) {
+      originRequest._retry = true;
+
+      try {
+        const { refreshToken, setTokens, logout } = useAuthStore.getState();
+        if (!refreshToken) {
+          alert('세션이 만료되었습니다. 다시 로그인해주세요.');
+          logout();
+          return Promise.reject(error);
+        }
+
+        const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
+          await apiRefreshToken(refreshToken);
+
+        setTokens({
+          accessToken: newAccessToken,
+          refreshToken: newRefreshToken,
+        });
+
+        originRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+
+        return api(originRequest);
+      } catch (refreshError) {
+        const { logout } = useAuthStore.getState();
+        alert('세션 갱신 실패, 다시 로그인해주세요');
+        logout();
+        return Promise.reject(refreshError);
+      }
+    }
     return Promise.reject(error);
   },
 );
