767ac5e2a0014cb8a21ae25721433717f9506cec
TopiaYu <raemo963@naver.com>
2025-07-16 16:08:03 +0900

    ✨ Feat: 로그인 페이지 로직 및 간단한 ui 구현
    - app/(auth)에서 login 페이지를 만들고 LoginForm을 불러옵니다
    - feature/auth/.../login에서 LoginForm.tsx 생성
    - react-hook-form, zod 사용
    - api 호출, 2토큰을 로컬스토리지에 저장

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from '@/features/auth/login/components/LoginForm';
+
+const Login = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+};
+
+export default Login;
