765165329e877e73886e9ad0dcf02dd099fc1d36
Hyun <ikimyh059554@gmail.com>
2025-07-16 17:11:03 +0900

    ✨ feat: 메인화면 라우팅 페이지 추가 (배너 캐러셀, 서치, 인기, 모든 액티티비)

--- src/app/activities/page.tsx
+++ src/app/activities/page.tsx
@@ -1,4 +1,17 @@
+import AllActivities from '@/features/activities/components/all-activities';
+import BannerCarousel from '@/features/activities/components/banner-carousel';
+import BestActivities from '@/features/activities/components/best-activities';
+import Search from '@/features/activities/components/search';
+
 const ActivitiesPage = () => {
-  return <div>ActivitiesPage</div>;
+  return (
+    <main className="bg-background flex w-full flex-col gap-10">
+      <BannerCarousel />
+      <Search />
+      <BestActivities />
+      <AllActivities />
+    </main>
+  );
 };
+
 export default ActivitiesPage;
