4683bb4868ab3a9e7a3b8fa163009bdaf5b878a2
Hyun <ikimyh059554@gmail.com>
2025-07-16 22:16:29 +0900

    ♻️ refactor: src/app layout 추가로 page.tsx의 헤더, 푸터 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 'use client';
 
 import LandingPage from '@/features/landing/landing-page';
-import Footer from '@/shared/components/footer';
-import Header from '@/shared/components/header';
 
 const Page = () => {
   return (
-    <>
-      <Header />
-      <main>
-        <LandingPage />
-      </main>
-      <Footer />
-    </>
+    <main>
+      <LandingPage />
+    </main>
   );
 };
 
