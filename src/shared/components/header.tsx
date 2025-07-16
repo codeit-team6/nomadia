6088409db0da8c8b5218521418d2f2a97dda3201
Hyun <ikimyh059554@gmail.com>
2025-07-16 11:01:24 +0900

    ♻️ refactor: 원인 불명의 warning 표시로 인해 저장 눌러서 정렬

--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -1,26 +1,22 @@
 'use client';
 
-import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import React from 'react';
 
 const Header: React.FC = () => {
   const isLoggedIn = false; // 임시코드 (로그인, 비로그인 확인용)
 
   return (
-    <nav className="w-full max-w-[192rem] mx-auto h-[4.8rem] md:h-[6rem] px-[2.4rem] py-[0.6rem] md:px-[3rem] md:py-[1rem] lg:px-[20rem] bg-sub flex justify-between items-center">
-     
+    <nav className="bg-sub mx-auto flex h-[4.8rem] w-full max-w-[192rem] items-center justify-between px-[2.4rem] py-[0.6rem] md:h-[6rem] md:px-[3rem] md:py-[1rem] lg:px-[20rem]">
       <div className="text-main txt-20-bold cursor-pointer md:flex md:gap-3">
-        <Image
-          src="/images/icons/logo.svg"
-          alt="Logo"
-          width={29}
-          height={24}
-        />
-        <span className='text-[#188FFF] txt-20-bold hidden md:block'>NOMADIA</span>
+        <Image src="/images/icons/logo.svg" alt="Logo" width={29} height={24} />
+        <span className="txt-20-bold hidden text-[#188FFF] md:block">
+          NOMADIA
+        </span>
       </div>
-      
-      <ul className="text-gray-950 txt-14-medium flex space-x-12">
+
+      <ul className="txt-14-medium flex space-x-12 text-gray-950">
         {isLoggedIn ? (
           <>
             {/* 로그인 상태일 때 */}
@@ -32,28 +28,32 @@ const Header: React.FC = () => {
                 height={19}
               />
             </li>
-            <span className='text-gray-100'>|</span>
-            <li className='flex items-center gap-2'>
+            <span className="text-gray-100">|</span>
+            <li className="flex items-center gap-2">
               <Image
                 src="/images/icons/profile.svg"
                 alt="프로필"
                 width={24}
                 height={24}
-                className="w-6 h-6 rounded-full"
+                className="h-6 w-6 rounded-full"
               />
-              <span className="text-gray-950 txt-14-medium">정만철</span>
+              <span className="txt-14-medium text-gray-950">정만철</span>
             </li>
           </>
         ) : (
           <>
             {/* 비로그인 상태일 때 */}
-        <li>
-          <Link href="/login" className="">로그인</Link>
-        </li>
-        <li>
-          <Link href="/signup" className="">회원가입</Link>
-        </li>
-        </>
+            <li>
+              <Link href="/login" className="">
+                로그인
+              </Link>
+            </li>
+            <li>
+              <Link href="/signup" className="">
+                회원가입
+              </Link>
+            </li>
+          </>
         )}
       </ul>
     </nav>
