9db96a49cfe4fd6f30b734aa6898b6c49a922d4c
Hyun <ikimyh059554@gmail.com>
2025-07-17 22:04:26 +0900

    📃 docs: jsdoc 추가

--- src/shared/libs/hooks/useActivityQuery.ts
+++ src/shared/libs/hooks/useActivityQuery.ts
@@ -6,7 +6,13 @@ import {
   GetActListApiResponse,
 } from '@/features/activities/libs/types/activity';
 
-// 랜딩페이지, 메인화면 배너, 인기 체험 컴포넌트에서 사용될 useActivity 훅
+/**
+ * 체험 목록 조회 훅
+ * @author 김영현
+ * @param params - 체험 목록 조회 파라미터
+ * @returns 체험 목록 조회 결과
+ * @description 랜딩페이지, 메인화면 배너, 인기 체험 컴포넌트에서 사용될 useActivity 훅
+ */
 const useActivity = (params: GetActListApiParams) => {
   return useQuery<GetActListApiResponse>({
     queryKey: ['activities', params],
