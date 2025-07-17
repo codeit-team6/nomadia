dcda95f97bb68e69343de44353f9a5de14bf9d92
jyn <spdlqjdkslel1226@naver.com>
2025-07-18 02:10:27 +0900

    ♻️ 🐰 refactor: useModalStore파일 내 타입 안전성 강화

--- src/shared/libs/stores/useModalStore.ts
+++ src/shared/libs/stores/useModalStore.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 
 interface ModalState {
   isModalOpen: boolean;
-  modalType: string;
+  modalType: 'confirm' | 'warning' | 'custom';
   openModal: () => void;
   closeModal: () => void;
   setModalType: (type: 'confirm' | 'warning' | 'custom') => void;
@@ -13,5 +13,6 @@ export const useModalStore = create<ModalState>((set) => ({
   modalType: 'confirm',
   openModal: () => set({ isModalOpen: true }),
   closeModal: () => set({ isModalOpen: false }),
-  setModalType: (type: string) => set({ modalType: type }),
+  setModalType: (type: 'confirm' | 'warning' | 'custom') =>
+    set({ modalType: type }),
 }));
