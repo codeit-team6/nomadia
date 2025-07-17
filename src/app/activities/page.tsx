e817238e3aa83a8bd669d0f2379073a642785840
Hyun <ikimyh059554@gmail.com>
2025-07-17 13:05:11 +0900

    📃 docs: 준우님이 개발하실 search ui 부분

--- src/app/activities/page.tsx
+++ src/app/activities/page.tsx
@@ -1,13 +1,12 @@
 import AllActivities from '@/features/activities/components/all-activities';
 import BannerCarousel from '@/features/activities/components/banner-carousel';
 import BestActivities from '@/features/activities/components/best-activities';
-import Search from '@/features/activities/components/search';
 
 const ActivitiesPage = () => {
   return (
     <main className="bg-background flex w-full flex-col gap-10">
       <BannerCarousel />
-      <Search />
+      {/* <Search /> */}
       <BestActivities />
       <AllActivities />
     </main>
---
+++ src/features/activities/components/search.tsx
@@ -0,0 +1,12 @@
+const Search = () => {
+  return (
+    <div className="flex-center flex-col gap-4 rounded-2xl border border-gray-300 p-4">
+      <p className="text-[1.6rem] font-bold">무엇을 체험하고 싶으신가요?</p>
+      <div className="flex-center flex-col gap-2">
+        <div>준우님이 개발하시는 부분</div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
