eaa425ceec51e4db9cc65af1706ec2384f21a1e1
jyn <71581298+dkslel1225@users.noreply.github.com>
2025-07-17 13:51:15 +0900

    [✨Feat] 공통 컴포넌트 - 페이지네이션 (#26)
    * 🎨 style: globals.css - 브랜드 컬러 수정
    
    * ✨ feat: pagination 공통 컴포넌트 구현
    
    * 📝 docs: 템플릿 수정, JSDoc 작성
    
    * 📝 docs: JSDoc 수정
    
    * ♻️🐰 refactor: 스크린 리더를 위한 ARIA 레이블 작성, totalPages가 0인 경우에 대한 예외처리
    
    ---------
    
    Co-authored-by: jyn <spdlqjdkslel1226@naver.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import LandingPage from '@/features/landing/landing-page';
-import Header from '@/shared/components/header';
 import Footer from '@/shared/components/footer';
+import Header from '@/shared/components/header';
 
 export default function Page() {
   return (
     <>
-    <Header />
-    <main>
-      <LandingPage />
-    </main>
-    <Footer />
+      <Header />
+      <main>
+        <LandingPage />
+      </main>
+      <Footer />
     </>
   );
 }
