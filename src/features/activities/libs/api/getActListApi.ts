89d4eed2f900614e3db357b66c9942b8cf90cf68
Hyun <ikimyh059554@gmail.com>
2025-07-17 14:44:15 +0900

    ♻️ refactor: BASE_URL이 없는 경우 대응

--- src/features/activities/libs/api/getActListApi.ts
+++ src/features/activities/libs/api/getActListApi.ts
@@ -7,6 +7,10 @@ import {
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
+if (!BASE_URL) {
+  throw new Error('NEXT_PUBLIC_API_BASE_URL 환경 변수가 설정되지 않았습니다.');
+}
+
 const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
