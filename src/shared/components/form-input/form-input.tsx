c7070b672b2963b48bf87f3144917f8aabf78cd6
TopiaYu <167048286+TopiaYu@users.noreply.github.com>
2025-07-18 19:20:17 +0900

    [✨Feat] 인풋 공통 컴포넌트 구현 (#40)
    * ✨ Feat: input 공통 컴포넌트 구현
    
    - input 공통 컴포넌트를 구현합니다
    - 자잘한 수정(글씨)
    - LoginForm에 예시로 사용
    
    * ✨ Feat: 반응형 구현
    
    * 🐛 Fix: 자잘한 수정 및 제네릭 추가

---
+++ src/shared/components/form-input/form-input.tsx
@@ -0,0 +1,47 @@
+import {
+  FieldError,
+  FieldValues,
+  Path,
+  UseFormRegister,
+} from 'react-hook-form';
+
+/**
+ * @description react-hook-form 과 함께 쓰는 공통 입력 컴포넌트
+ *  * 제네릭을 사용하여 로그인, 회원가입 등 모든 폼에서 재사용할 수 있습니다. so hard,,,
+ * @param {string} label - 창 위에 표시될 라벨 텍스트
+ * @param {Path<T>} name - react-hook-form에 등록할 필드의 이름
+ * @param {UseFormRegister<T>} register - 부모 폼에서 전달받는 react-hook-form의 register 함수
+ * @param {FieldError} [errors] - 해당 필드의 유효성 검사 에러 객체
+ * @param {React.InputHTMLAttributes<HTMLInputElement>} ...rest - type, placeholder 등 표준 HTML input 속성을 그대로 전달받습니다.
+ */
+
+interface FormInputProps<T extends FieldValues>
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  name: Path<T>;
+  register: UseFormRegister<T>;
+  error?: FieldError;
+}
+
+export const FormInput = <T extends FieldValues>({
+  label,
+  name,
+  register,
+  error,
+  ...rest
+}: FormInputProps<T>) => {
+  return (
+    <div className="mb-5 flex flex-col">
+      <label htmlFor={name} className="txt-16-medium mb-3">
+        {label}
+      </label>
+      <input
+        id={name}
+        {...rest}
+        {...register(name)} // type, placeholder 등을 여기에 적용
+        className="txt-14-medium mb-1.5 h-13.5 w-full max-w-[328px] rounded-lg border px-3 md:max-w-[640px]"
+      />
+      {error && <p className="txt-12-medium text-red-500">{error.message}</p>}
+    </div>
+  );
+};
