9468c46617ed59cd65c716a7fb3aedf4b8af5b31
TopiaYu <167048286+TopiaYu@users.noreply.github.com>
2025-07-19 15:13:45 +0900

    [🎨 ✨ Style] 회원가입, 로그인 UI 구현 (#43)
    * ✨ Feat: auth의 공통 layout을 추가
    
    - login, signup의 공통 layout을 추가하였습니다.
    
    * 💄 Style: 회원가입 페이지 ui 구현
    
    - ui 구현, 반응형 구현
    
    * 💄 Style: 로그인 페이지 UI 구현
    
    * ✨ Feat: 눈아이콘 클릭시 비밀번호 보이기 유무 구현
    
    * 💄 Style: 로그인, 회원가입 UI 수정
    
    - 피그마 시안대로 UI를 수정합니다
    - 간편 회원가입은 없고 간편 로그인만 존재
    - 카카오로고 이미지 추가
    
    * 🐛 Fix: 오탈자 수정

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,49 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 import { LoginForm } from '@/features/auth/login/components/LoginForm';
 
 const Login = () => {
   return (
-    <>
+    <div className="mx-6 mt-9 mb-10 flex w-full max-w-[343px] flex-col items-center md:max-w-[460px]">
+      <Image
+        src="/images/icons/logo-desktop.png"
+        alt="Signup logo image"
+        width={86}
+        height={71}
+        className="mb-10 w-[86px] md:w-[120px]"
+      />
+
       <LoginForm />
-    </>
+
+      <p className="txt-16-medium mt-4 mb-12 text-gray-400">
+        아직 계정이 없으신가요?{' '}
+        <Link
+          href="/signup"
+          className="text-main txt-16-medium cursor-pointer underline"
+        >
+          가입하기
+        </Link>
+      </p>
+
+      <div className="my-6 flex w-full items-center">
+        <hr className="border-main flex-1" />
+        <span className="txt-16-medium text-main mx-4"> OR </span>
+        <hr className="border-main flex-1" />
+      </div>
+
+      <div className="flex w-full justify-between">
+        <p className="txt-16-medium text-gray-950">간편 로그인하기</p>
+        <button type="button" className="cursor-pointer">
+          <Image
+            src="/images/icons/logo-kakao.png"
+            alt="kakao image"
+            width={42}
+            height={42}
+          />
+        </button>
+      </div>
+    </div>
   );
 };
 
