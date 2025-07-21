60898d02c9cdfab5e7d94c615231fc409cacf2a3
wnsdn00 <114475584+wnsdn00@users.noreply.github.com>
2025-07-21 23:02:37 +0900

    [✨Feat] header-로그인/로그아웃 연결 (#58)
    * header-로그인/로그아웃 연결
    
    * 로그인 시 주소변경

--- src/features/auth/login/components/LoginForm.tsx
+++ src/features/auth/login/components/LoginForm.tsx
@@ -32,7 +32,7 @@ export const LoginForm = () => {
       const response = await apiLogin(data);
       storeLogin(response);
       toast.success('로그인 성공');
-      router.push('/');
+      router.push('/activities');
     } catch (error) {
       console.error('로그인 실패', error);
       toast.error('로그인에 실패했습니다');
