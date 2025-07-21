befa394b3730456491cf0d07734926acbaac49cc
jyn <71581298+dkslel1225@users.noreply.github.com>
2025-07-21 15:04:50 +0900

    [♻️Refactor] 에러 페이지 리팩토링 (#51)
    * ♻️ refactor: 에러페이지 리팩토링, 별도의 페이지 및 컴포넌트 없이 error.tsx에서 처리하도록 변경
    
    * ♻️🐰refactor: 코드리뷰 반영
    
    ---------
    
    Co-authored-by: jyn <spdlqjdkslel1226@naver.com>

---
+++ src/features/(test)/getTestApi.ts
@@ -0,0 +1,19 @@
+// 테스트 파일입니다. test-scroll
+import { testInstance } from '@/features/(test)/testInstance';
+import {
+  GetActListApiParams,
+  GetActListApiResponse,
+} from '@/features/activities/libs/types/activity';
+
+interface GetListWithCursorID extends GetActListApiParams {
+  cursorId?: number; // 요청 파라미터에 - 커서 항목 필수
+}
+
+export const getTestApi = async (
+  params?: GetListWithCursorID,
+): Promise<GetActListApiResponse> => {
+  const { data } = await testInstance.get('activities?method=cursor', {
+    params,
+  });
+  return data;
+};
