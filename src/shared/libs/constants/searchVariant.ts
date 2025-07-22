d8c854e926ad83a4452dcb65c9a114d242cdf24d
wnsdn00 <junwoo000706@gmail.com>
2025-07-22 13:03:53 +0900

    wrapper 간격 수정

--- src/shared/libs/constants/searchVariant.ts
+++ src/shared/libs/constants/searchVariant.ts
@@ -1,6 +1,7 @@
 export const searchVariant = {
   main: {
-    wrapper: 'flex-center flex-col gap-[1.2rem] py-[5rem] ',
+    wrapper:
+      'flex-center flex-col gap-[1.2rem] py-[1.6rem] md:py-[3.2rem] md:px-[4rem] md:gap-[3.6rem] mt-[1.2rem] lg:mt-[3rem]',
     title: 'font-bold text-[1.6rem] text-gray-950 md:text-[3.2rem] pb-[1rem]',
     inputBox:
       'flex h-[4.9rem] w-full items-center rounded-full bg-white border focus-within:border-[#3D9EF2] py-[0.6rem] pr-[0.8rem] pl-[2rem] shadow-md md:h-[7rem] md:w-[62.8rem] lg:w-[104rem] lg:py-[1rem] lg:pr-[1.2rem] lg:pl-[3.2rem]',
