03f5e5e04f12b18c0f29a60dff79eaea165b4a8d
jyn <71581298+dkslel1225@users.noreply.github.com>
2025-07-24 23:02:51 +0900

    [♻️Refactor] 모달 컴포넌트 리팩토링 (#81)
    * ♻️ refactor: 모달 공통 컴포넌트 리팩토링
    
    * 📝 docs: 주석 수정
    
    ---------
    
    Co-authored-by: jyn <spdlqjdkslel1226@naver.com>

--- src/shared/components/modal/components/modal-header.tsx
+++ src/shared/components/modal/components/modal-header.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from 'react';
 
+import { modalHeaderClasses } from '@/shared/components/modal/libs/modalClasses';
 import { cn } from '@/shared/libs/cn';
 import { useModalStore } from '@/shared/libs/stores/useModalStore';
 
 /**
- * Modal.Header
+ * @author 지윤
+ * @component Modal.Header
  *
  * 모달의 제목 또는 안내 문구를 표시하는 영역입니다.
  *
@@ -13,7 +15,6 @@ import { useModalStore } from '@/shared/libs/stores/useModalStore';
  *
  * @param {ReactNode} props.children - 표시할 텍스트 또는 요소
  */
-
 export const Header = ({ children }: { children: ReactNode }) => {
   const { modalType } = useModalStore();
 
@@ -21,9 +22,7 @@ export const Header = ({ children }: { children: ReactNode }) => {
     <p
       className={cn(
         'text-[1.6rem] font-semibold text-black md:text-[2rem]',
-        modalType === 'confirm' && 'mb-[1.4rem] md:mt-[0.6rem] md:mb-[1.6rem]',
-        modalType === 'warning' && 'mb-[2rem] md:mb-[2.4rem]',
-        modalType === 'custom' && 'mb-[1.2rem] md:mb-[1.6rem]',
+        modalHeaderClasses[modalType],
       )}
     >
       {children}
---
+++ src/shared/components/modal/libs/modalClasses.ts
@@ -0,0 +1,23 @@
+export const modalContentClasses: Record<string, string> = {
+  confirm:
+    'flex  w-[32rem] flex-col items-center p-[3rem] pt-[3.4rem] text-center h-fit md:w-[40rem] md:p-[4rem] md:pb-[3.4rem]',
+  warning:
+    'flex w-[32rem] flex-col items-center p-[3rem] pb-[2.4rem] text-center md:w-[40rem] md:p-[3rem]',
+  custom: 'px-[2.4rem] py-[3rem]',
+};
+
+export const modalHeaderClasses: Record<string, string> = {
+  confirm: 'mb-[1.4rem] md:mt-[0.6rem] md:mb-[1.6rem]',
+  warning: 'mb-[2rem] md:mb-[2.4rem]',
+  custom: 'mb-[1.2rem] md:mb-[1.6rem]',
+};
+
+export const modalButtonClasses: Record<string, string> = {
+  confirm: 'w-[18rem] md:w-[20rem] h-[4.1rem] md:h-[4.7rem]',
+  warning: 'w-[11.3rem] md:w-[13.5rem] h-[4.1rem] md:h-[4.7rem]',
+  custom: 'w-full md:mb-[2rem] h-[4.1rem] md:h-[5.4rem] ',
+};
+export const modalButtonColorClasses: Record<string, string> = {
+  white: 'border border-gray-200 bg-white font-medium text-gray-600',
+  blue: 'bg-main font-semibold text-white',
+};
