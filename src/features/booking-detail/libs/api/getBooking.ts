be5c89484ef5cd54d84b290c1713a89d71ce2aaa
Hyun <ikimyh059554@gmail.com>
2025-07-26 15:32:06 +0900

    📃docs: jsdoc 작성

--- src/features/booking-detail/libs/api/getBooking.ts
+++ src/features/booking-detail/libs/api/getBooking.ts
@@ -1,6 +1,12 @@
 import { GetBookingParams } from '@/features/booking-detail/libs/types/booking';
 import api from '@/shared/libs/api/axios';
 
+/**
+ * 예약 목록 조회 API
+ * @author 김영현
+ * @param params - 예약 목록 조회 파라미터
+ * @returns 예약 목록
+ */
 export const getBooking = async (params?: GetBookingParams) => {
   const { data } = await api.get(`/my-reservations`, { params });
   return data;
