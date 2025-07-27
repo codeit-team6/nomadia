86f8bebc2fd7dcb094b72d056ae69b486d7d1ad7
wnsdn00 <114475584+wnsdn00@users.noreply.github.com>
2025-07-27 19:36:47 +0900

    [✨Feat] 간편로그인 구현 (#83)
    * 간편로그인 구현
    
    * 코드 리팩토링 및 파일구조 수정
    
    * 주소값 수정 및 구조변경

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 
 import { LoginForm } from '@/features/auth/login/components/LoginForm';
+import handleKakaoLogin from '@/features/auth/utils/kakao-login-handler';
 
 const Login = () => {
   return (
@@ -36,7 +39,11 @@ const Login = () => {
 
       <div className="flex w-full justify-between">
         <p className="txt-16-medium text-gray-950">간편 로그인하기</p>
-        <button type="button" className="cursor-pointer">
+        <button
+          type="button"
+          className="cursor-pointer"
+          onClick={handleKakaoLogin}
+        >
           <Image
             src="/images/icons/logo-kakao.png"
             alt="kakao image"
