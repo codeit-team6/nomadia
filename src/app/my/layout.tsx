656cdd37a7dd57a232dbabf476b4fcd12490995e
Hyun <ikimyh059554@gmail.com>
2025-07-29 14:21:24 +0900

    ✨ feat: 모바일일 때 뒤로가기 버튼 기능 추가

--- src/app/my/layout.tsx
+++ src/app/my/layout.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import React, { useEffect } from 'react';
 
@@ -21,8 +22,28 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const isActivityRegistration =
     pathname === '/my/my-activities/activity-registration';
 
+  const handleBackClick = () => {
+    router.back();
+  };
+
   return (
     <div className="mx-auto flex justify-center px-[2.4rem] py-[3rem] md:max-w-[68.4rem] md:gap-[3rem] lg:max-w-[98rem] lg:gap-[5rem]">
+      {/* 모바일에서만 보이는 뒤로가기 버튼 */}
+      {isMobile && !isMyPageRoot && (
+        <button
+          onClick={handleBackClick}
+          className="absolute top-[5rem] left-[2.4rem]"
+          aria-label="뒤로 가기"
+        >
+          <Image
+            src="/images/icons/back.svg"
+            alt="뒤로 가기"
+            width={23}
+            height={23}
+          />
+        </button>
+      )}
+
       <aside
         className={
           (isMobile && !isMyPageRoot) || isActivityRegistration ? 'hidden' : ''
