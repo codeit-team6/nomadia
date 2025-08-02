35edbf23f7fdb5694130ba0f2a7c83de9b00b2ff
jyn <spdlqjdkslel1226@naver.com>
2025-08-02 17:50:59 +0900

    🐛 fix: 체험상세페이지 데이터 호출 실패 시 not found로 라우팅 & 데이터 로딩중에 !data 조건 추가함 - 404로 리다이렉트 시 넘어가는 중간과정에서 먼저 그려진 ui가 보이는 어색함이 있었음. —> !data추가해서 데이터가 비어있지 않은 시점을 잡아서 해결.

--- src/features/activityId/libs/hooks/useActivityIdQuery.ts
+++ src/features/activityId/libs/hooks/useActivityIdQuery.ts
@@ -3,12 +3,11 @@ import { ParamValue } from 'next/dist/server/request/params';
 
 import { getActivityId } from '@/features/activityId/libs/api/getActivityId';
 
-//staleTime - 예약현황 업데이트 빈도: e.g.숙소 앱은 1~3분 & 예약 직전 실시간 확인 refetch()
 export const useActivityIdQuery = (id: ParamValue) => {
   return useQuery({
     queryKey: ['activityId', id],
     queryFn: () => getActivityId(id),
-    staleTime: 1000 * 60 * 5,
+    staleTime: 1000 * 60 * 30,
     retry: 1,
   });
 };
