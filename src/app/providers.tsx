9537f6aeb3d0eaba2c2980ba5a159043f934fec6
TopiaYu <raemo963@naver.com>
2025-08-04 01:49:01 +0900

    ♻️ Refactor: 서버가 새로 시작하면 로컬스토리지 내역 삭제를 통해 로그아웃 처
    - 서버 내렸다가 다시 킬때 로그아웃 처리

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { Toaster } from 'sonner';
 
 export function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'development') {
+      localStorage.removeItem('auth-storage');
+    }
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
