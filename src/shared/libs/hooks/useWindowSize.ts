c3903ce5c345b8aeedb0f2dba4ab5eff913ec686
jyn <71581298+dkslel1225@users.noreply.github.com>
2025-08-18 12:46:49 +0900

    [♻️Refactor] 화면 너비값 관리 (resize event) (#167)
    * ♻️ refactor: useWindowSize.ts에 화면 사이즈 상태값 추가 및 관리
    
    * 🐰refactor: SSR환경에서의 React hydration mismatch 방지하기 위해 ready 상태값 추가힘
    - isDesktop등을 반영하는 스타일이 있는 경우 + SSR방식으로 렌더링 되는 경우 -> ready여부 체크하여 렌더링 에러 방지하는걸 권장함
    
    ---------
    
    Co-authored-by: jyn <spdlqjdkslel1226@naver.com>

--- src/shared/libs/hooks/useWindowSize.ts
+++ src/shared/libs/hooks/useWindowSize.ts
@@ -3,27 +3,41 @@ import { useEffect, useState } from 'react';
 interface WindowSize {
   width: number | undefined;
   height: number | undefined;
+  isDesktop: boolean;
+  isTablet: boolean;
+  isMobile: boolean;
+  ready: boolean;
 }
 
 const useWindowSize = (): WindowSize => {
   const [windowSize, setWindowSize] = useState<WindowSize>({
     width: undefined,
     height: undefined,
+    isDesktop: false,
+    isTablet: false,
+    isMobile: false,
+    ready: false,
   });
 
   useEffect(() => {
     if (typeof window === 'undefined') return;
 
     const handleResize = () => {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
       setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width,
+        height,
+        isDesktop: width >= 1024,
+        isTablet: width >= 768 && width < 1024,
+        isMobile: width < 768,
+        ready: true,
       });
     };
-    window.addEventListener('resize', handleResize);
 
-    handleResize();
-    return () => window.removeEventListener('resize', handleResize);
+    handleResize(); // 초기 실행
+    window.addEventListener('resize', handleResize); // resize 이벤트 추적(등록)
+    return () => window.removeEventListener('resize', handleResize); // 언마운트 시 해제
   }, []);
   return windowSize;
 };
