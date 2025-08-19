ecd721a0117546f84ff12a290b9708ecc33b1e3f
wnsdn00 <junwoo000706@gmail.com>
2025-08-20 00:36:15 +0900

    폴더구조 수정

--- src/features/reservation-state/components/empty-reservation.tsx
+++ src/features/reservation-state/components/empty-reservation.tsx
@@ -4,10 +4,10 @@ const EmptyReservation = () => {
   return (
     <div className="flex h-full flex-col items-center text-center">
       <CalendarDays className="text-main mt-15 h-12 w-12" />
-      <h2 className="text-bold mt-20 text-[2rem] text-gray-900">
+      <h2 className="text-bold mt-20 text-[2.2rem] text-gray-700">
         해당 날짜에는 예약내역이 없어요.
       </h2>
-      <p className="text-medium mt-10 text-[1.8rem] text-gray-700">
+      <p className="text-medium mt-10 text-[1.8rem] text-gray-500">
         예약한 날짜를 선택하면,
         <br />
         내 체험을 예약 신청한 사람들과
