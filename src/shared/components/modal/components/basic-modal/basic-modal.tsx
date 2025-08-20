671b89af9ae065b2040e63067719ae19e985b94d
jyn <spdlqjdkslel1226@naver.com>
2025-08-21 03:44:55 +0900

    🎨 style: basic-modal 오픈 애니메이션 추가

--- src/shared/components/modal/components/basic-modal/basic-modal.tsx
+++ src/shared/components/modal/components/basic-modal/basic-modal.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { AnimatePresence, motion } from 'framer-motion';
 import { ReactNode, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
@@ -36,21 +37,35 @@ const BasicModal = ({
 
   if (isModalOpen) {
     return createPortal(
-      <>
-        {/* 오버레이 */}
-        <div
-          className="flex-center fixed inset-0 z-100 bg-black/50"
-          onClick={() => {
-            closeModal();
-          }}
-          role="presentation"
-        >
-          {/* 모달 */}
-          <ModalContent type={type} extraClassName={extraClassName}>
-            {children}
-          </ModalContent>
-        </div>
-      </>,
+      <AnimatePresence>
+        {isModalOpen && (
+          <>
+            {/* 오버레이 */}
+            <motion.div
+              className="flex-center fixed inset-0 z-100 bg-black/50"
+              onClick={() => {
+                closeModal();
+              }}
+              role="presentation"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.2, ease: 'easeInOut' }}
+            >
+              <motion.div
+                initial={{ scale: 0.8 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0.8 }}
+              >
+                {/* 모달 */}
+                <ModalContent type={type} extraClassName={extraClassName}>
+                  {children}
+                </ModalContent>
+              </motion.div>
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>,
       document.body,
     );
   }
