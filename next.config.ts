731ce2b94734647667016edf8b9effe016f9d896
Hyun <ikimyh059554@gmail.com>
2025-08-28 16:01:14 +0900

    ♻️ refactor: 복잡한 webpack 설정 제거하고 Next.js 기본 최적화 활용

--- next.config.ts
+++ next.config.ts
@@ -21,36 +21,6 @@ const nextConfig: NextConfig = {
     // 큰 패키지만 최적화
     optimizePackageImports: ['swiper', '@/shared/components'],
   },
-
-  // webpack 번들 최적화
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      // 프로덕션 빌드 최적화
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        cacheGroups: {
-          // 공통 컴포넌트들을 하나의 청크로 묶기
-          shared: {
-            test: /[\\/]src[\\/]shared[\\/]components[\\/]/,
-            name: 'shared-components',
-            chunks: 'all',
-            priority: 10,
-          },
-          // vendor 청크 (node_modules)
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all',
-            priority: 5,
-          },
-        },
-      };
-    }
-    return config;
-  },
-
-  // 압축 최적화
-  compress: true,
 };
 
 export default nextConfig;
