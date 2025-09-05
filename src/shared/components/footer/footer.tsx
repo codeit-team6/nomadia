8d359ed01d4a5c2be2d3785fa05b9a4c8a94ddb0
Hyun <ikimyh059554@gmail.com>
2025-09-05 22:21:27 +0900

    ♻️ refactor: footer 크롤링 개선

--- src/shared/components/footer/footer.tsx
+++ src/shared/components/footer/footer.tsx
@@ -6,21 +6,10 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="txt-13-medium mx-auto h-[11.6rem] w-full border-t border-gray-100 bg-white px-[2.4rem] py-[3rem] text-gray-600 md:h-[14rem] md:px-[4rem] lg:px-[20rem]">
-      {/* 모바일 사이즈: 위에 보일 Privacy Policy · FAQ */}
-      <div className="mb-4 flex justify-center gap-7 md:hidden">
-        <a>Privacy Policy</a>
-        <span>·</span>
-        <a>FAQ</a>
-      </div>
-
       <div className="flex flex-row items-center justify-between md:grid md:grid-cols-3 md:items-center">
         <div className="text-left text-gray-400">nomadia - 2025</div>
 
-        <div className="hidden justify-center gap-4 md:flex">
-          <a>Privacy Policy</a>
-          <span>·</span>
-          <a>FAQ</a>
-        </div>
+        <div className="hidden justify-center gap-4 md:flex"></div>
 
         <div className="flex items-center justify-end gap-[1.6rem]">
           <a
